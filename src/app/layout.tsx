6f968d2338be50a52405a50091816f54eb65e93d
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-06-15 00:05:44 +0330

    feat: add top loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import resolveConfig from "tailwindcss/resolveConfig";
+import NextTopLoader from "nextjs-toploader";
 
 import "./globals.css";
+import tailwindConfig from "../../tailwind.config";
 import { NextAuthSessionProvider, ReactQueryProvider } from "@/providers";
 
 const nunito = Nunito({ subsets: ["latin"] });
+
+const fullConfig = resolveConfig(tailwindConfig);
 
 export const metadata: Metadata = {
   title: "Fountain",
@@ -19,6 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
+        <NextTopLoader
+          shadow={false}
+          color={fullConfig.theme.colors.indigo["600"]}
+          showSpinner={false}
+        />
         <NextAuthSessionProvider>
           <ReactQueryProvider>{children}</ReactQueryProvider>
         </NextAuthSessionProvider>
